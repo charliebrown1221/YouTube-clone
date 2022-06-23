47fe17ac2d0a8ea7bff6fc3d59ee37edd8ff8192
charliebrown1221 <chaimbrown1@yahoo.com>
2022-06-23 12:55:27 -0400

    created models in comment

--- backend/comment/models.py
+++ backend/comment/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from reply.models import Reply
 # Create your models here.
+class Comment(models.Model):
+    user = models.ForeignKey(Reply, on_delete=models.CASCADE)
+    video_id = models.CharField(max_length=500)
+    text = models.CharField(max_length=500)
+    likes = models.IntegerField()
+    dislikes = models.IntegerField()
