1a76f29b31652893247caee70b7b119420622081
charliebrown1221 <chaimbrown1@yahoo.com>
2022-06-23 17:36:55 -0400

    added apps to workbench

--- backend/comment/models.py
+++ backend/comment/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from reply.models import Reply
+from authentication.models import User
 # Create your models here.
 class Comment(models.Model):
-    user = models.ForeignKey(Reply, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     video_id = models.CharField(max_length=500)
     text = models.CharField(max_length=500)
     likes = models.IntegerField()
--- backend/comment/views.py
+++ backend/comment/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from pyexpat import model
 from rest_framework import status
 from rest_framework.response import Response
@@ -9,15 +10,16 @@ from .serializers import CommentSerializer
 # Create your views here.
 @api_view(['GET'])
 @permission_classes([AllowAny])
-def get_all_comments(request):
-  if  request.method == 'GET':
-    comment = Comment.video_id.all()
-    serializer = CommentSerializer(comment, many=True)
+def get_all_comments(request, pk):
+    videoId=get_object_or_404(pk=video_id )
+    if  request.method == 'GET':
+       comment = Comment.object.all()
+    serializer = CommentSerializer(comment,videoId, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
 
 @api_view([ 'POST','PUT'])
 @permission_classes([IsAuthenticated])
-def user_cars(request):
+def user_comment(request):
     print(
         'User ', f"{request.user.id} {request.user.email} {request.user.username}")
     if request.method == 'POST':
