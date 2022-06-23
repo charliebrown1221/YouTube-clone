416960882875dc0afe3a8d23e0511c3a26046deb
charliebrown1221 <chaimbrown1@yahoo.com>
2022-06-23 15:55:25 -0400

    setup post and put with auth, not tested

--- backend/comment/views.py
+++ backend/comment/views.py
@@ -13,5 +13,20 @@ def get_all_comments(request):
   if  request.method == 'GET':
     comment = Comment.video_id.all()
     serializer = CommentSerializer(comment, many=True)
-    return Response(serializer.data)
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
+@api_view([ 'POST','PUT'])
+@permission_classes([IsAuthenticated])
+def user_cars(request):
+    print(
+        'User ', f"{request.user.id} {request.user.email} {request.user.username}")
+    if request.method == 'POST':
+         serializer = CommentSerializer(data=request.data)
+         if serializer.is_valid():
+            serializer.save(user=request.user)
+         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    elif request.method == 'PUT':
+        serializer= CommentSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user=request.user)
+        return Response (serializer.data, status=status.HTTP_200_OK)
