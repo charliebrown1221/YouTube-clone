383546d8f0aab921183fe7a7729fe66df693547f
charliebrown1221 <chaimbrown1@yahoo.com>
2022-06-23 14:21:22 -0400

    created models in reply (have some errors)

--- backend/reply/models.py
+++ backend/reply/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from comment.models import Comment
 # Create your models here.
+class Reply(models.Model):
+    user = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    text = models.CharField(max_length=500)
