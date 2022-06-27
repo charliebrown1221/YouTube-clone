ed246043d5b5a7e423700dab45051e3613ca4b30
charliebrown1221 <chaimbrown1@yahoo.com>
2022-06-27 18:08:59 -0400

    started axios get all

--- frontend/src/pages/SearchPage/SearchPage.jsx
+++ frontend/src/pages/SearchPage/SearchPage.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
+import axios from "axios";
 
 
 const SearchPage = (props) => {
- 
+    const [user, token] = useAuth();
+    const [coments, setComments] = useState([]);
+
+  useEffect(() => {
+    const getComments = async () => {
+        try {
+          let response = await axios.get("http://127.0.0.1:8000/api/comments/", {
+            headers: {
+              Authorization: "Bearer " + token,
+            },
+          });
+          setComments(response.data);
+        } catch (error) {
+          console.log(error.response.data);
+        }
+      };
+      getComments();
+    }, [token]);
 
     return ( 
         <div></div>
