93e2e44cae38fcd14045d4015a121f97b6201b89
charliebrown1221 <chaimbrown1@yahoo.com>
2022-06-28 18:10:23 -0400

    searchbar

---
+++ frontend/src/components/SearchBar.jsx
@@ -0,0 +1,26 @@
+import React,{useState} from "react";
+
+const SearchBar = (props) => {
+   
+   const[saveSearch,setSaveSearch]=useState()
+   
+   
+   function handelSearch(event){
+    event.preventDefault();
+    saveSearch(props.searchResults)
+   }
+   
+   
+   
+    return ( 
+   <form onSubmit={handelSearch}>
+        <div > 
+      <input type='text' value={saveSearch} onChange={(event)=> setSaveSearch(event.target.value)} ></input>
+      {console.log('savedSearch:',saveSearch)}
+      <button className='' type='submit' placeholder='What video are you looking for'>Search</button>
+        </div>
+        </form>
+     );
+}
+ 
+export default SearchBar;
--- frontend/src/pages/SearchPage/SearchPage.jsx
+++ frontend/src/pages/SearchPage/SearchPage.jsx
@@ -1,13 +1,15 @@
 import React,{useEffect,useState} from "react";
 import axios from "axios";
 import CommentMapper from "../../components/Comment/CommentMapper";
-import VideoPlayer from "../../components/VideoPlayer";
-import Navbar from "../../components/NavBar/NavBar";
+import SearchBar from "../../components/SearchBar";
+
 
 
 const SearchPage = (props) => {
     // const [user, token] = useAuth();
     const [comments, setComments] = useState([]);
+    const [searchResults, setSearchResults ]=useState([])
+    // const [videoId, setVideoId]=useState('')
 
   useEffect(() => {
     const getComments = async () => {
@@ -20,13 +22,31 @@ const SearchPage = (props) => {
         }};
       getComments();
     }, []);
+    
+    useEffect(() => {
+      const runSearch =async ()=>{
+      try {
+        let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=${searchResults}&key=AIzaSyBxIKxXVF3XT_WlqGfZSmlyBKhRkRmG_xE&type=video&part=snippet`)
+        console.log(response.data)
+        setSearchResults(response.data.items)
+      } catch (error) {
+        console.log(error.response.data)
+      }
+    };
+    runSearch();
+  },[]);
+
+
+     
+    
    
 
     return ( 
       <div>
         
         <div>
-          {/* <VideoPlayer /> */}
+          <SearchBar setSearchResults={setSearchResults} searchResults={searchResults}/>
+          
         </div>
       
         <div>
